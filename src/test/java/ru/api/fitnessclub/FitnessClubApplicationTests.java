175abaf73625fcda2c42dfa1884b01a5d3ec5887
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-05-21 20:34:14 +0300

    initial

---
+++ src/test/java/ru/api/fitnessclub/FitnessClubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.api.fitnessclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
